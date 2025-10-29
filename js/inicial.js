f241ed064c8db2886d77279df6edcf3294eeb953
Esther Barbosa <estherbguimaraes@gmail.com>
2025-10-29 07:12:59 -0300

    teste

--- js/inicial.js
+++ js/inicial.js
@@ -1,24 +1,22 @@
-const slide = document.querySelector('.carousel-slide');
-const items = document.querySelectorAll('.carousel-item');
-const prev = document.querySelector('.prev');
-const next = document.querySelector('.next');
+import Swiper from 'swiper';
+import 'swiper/css';
+import { Navigation, Pagination, Autoplay } from 'swiper/modules';
 
-let index = 0;
 
-// Função para mudar slide
-function showSlide(n) {
-  if (n >= items.length) index = 0;
-  else if (n < 0) index = items.length - 1;
-  else index = n;
-  
-  slide.style.transform = `translateX(${-index * 100}%)`;
-}
 
-// Botões
-next.addEventListener('click', () => showSlide(index + 1));
-prev.addEventListener('click', () => showSlide(index - 1));
+const swiper = new Swiper('.swiper', {
+  modules: [Navigation, Pagination, Autoplay],
+  loop: true,
+  autoplay: {
+    delay: 3000,
+  },
+  pagination: {
+    el: '.swiper-pagination',
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
 
-// Automático
-setInterval(() => {
-  showSlide(index + 1);
-}, 4000);
+
